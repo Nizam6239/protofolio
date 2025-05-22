dfeca39abfb70d3d83e4c175006d8a2dce23c13e
Nizam6239 <nizammalik6239@gmail.com>
2025-05-22 19:48:34 +0530

    chagne

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -47,7 +47,7 @@ export function HeroSection() {
 
       <div className="flex space-x-4 justify-center">
         <button 
-          className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+          className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
           type="button"
           onClick={() => handleExternalLink(SOCIAL_LINKS.linkedin)}
         >
@@ -59,11 +59,11 @@ export function HeroSection() {
 
         <a href="/NIZAM_Resume.pdf" download="Nizam_Resume.pdf">
           <button 
-            className="relative inline-flex h-12 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
+            className="relative inline-flex h-10 overflow-hidden rounded-full p-[1px] focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
             type="button"
           >
             <span className="absolute inset-[-1000%] animate-[spin_2s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#E2CBFF_0%,#393BB2_50%,#E2CBFF_100%)]" />
-            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-6 py-1 text-base font-medium text-white backdrop-blur-3xl">
+            <span className="inline-flex h-full w-full cursor-pointer items-center justify-center rounded-full bg-slate-950 px-4 py-[1px] text-base font-medium text-white backdrop-blur-3xl">
               {HERO_CONTENT.resumeText}
             </span>
           </button>
